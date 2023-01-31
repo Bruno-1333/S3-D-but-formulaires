b6e7d16feb6e588a8c61c8bba82c4b6543497804
Bruno-1333 <2295132rcegepoutaouais.qc.ca>
2023-01-31 11:39:49 -0500

    version 31/01/2023

--- main.js
+++ main.js
@@ -1,51 +1,51 @@
-// pegar o formulário pelo id
-const form = document.getElementById("contact");
+let province = document.querySelector('#province');
+let telephone = document.querySelector('#Telephone');
+let patate = document.querySelector('#patate');
+let pomme = document.querySelector('#pomme');
 
-    // adicionar um evento de envio ao formulário
-    // evitar que a página seja recarregada ao enviar o formulário
-    // aqui você pode adicionar sua lógica de validação de formulário e envio de dados
+/**
+ *  * pour afficher le nombre de patate dans range
+ */
+function afficher() {
+    let valeur = document.forms[0]["patates"].value;
+    document.getElementById("valeur").innerText = valeur;
+}
 
-    // exibir uma mensagem de sucesso
+function differencePommPatate(patate = "", pomme = "") {
+    return parseInt(patate, 10) > parseInt(pomme, 10);
+}
 
-    // Selecione o formulário pelo ID
+function choixTelProvince(telephone = "819", province = "Quebec") {
+    let regex = /\(?819(\) |-)?[0-9]{3}-?[0-9]{4}/;
+    switch (province) {
+        case "on":
+            regex = /\(?613(\) |-)?[0-9]{3}-?[0-9]{4}/;
+            break;
+        case "mb":
+            regex = /\(?(204|431)(\) |-)?[0-9]{3}-?[0-9]{4}/
+            break;
+    }
+    return regex.test(telephone);
+}
 
-// Adicione um ouvinte de evento de submit ao formulário
-    form.addEventListener("submit", function(event) {
-        // Prevenir o comportamento padrão de submit (recarregar a página)
-        event.preventDefault();
+document.querySelector("form").addEventListener('submit', function (evenement) {
+    //réinitialiser le message d,erreur
+    document.querySelectorAll(".erreur").forEach(message => message.classList.add("invisible"));
 
-        // Validação de formulário (verificar se todos os campos obrigatórios estão preenchidos)
-        var nome = document.getElementsByName("Nome")[0].value;
-        var email = document.getElementsByName("Courriel")[0].value;
-        var telefone = document.getElementsByName("Téléphone")[0].value;
-        var company = document.getElementsByName("Compagnie")[0].value;
-        var province = document.getElementsByName("province")[0].value;
-        var Pommes = document.getElementsByName("Pommes")[0].value;
-        var Patates = document.getElementsByName("Patates")[0].value;
-        var Pseudonyme = document.getElementsByName("Pseudonyme")[0].value;
-        var Commentaires = document.getElementsByName("Commentaires")[0].value;
-        if (nome == "" || email == "" || telefone == "" || company == "" || province == "" || Pommes == "" || Patates == "" || Pseudonyme == "" || Commentaires == "") {
-            alert("Todos os campos obrigatórios devem ser preenchidos");
-            return;
-        }
-        // Envio de dados (pode ser feito com uma chamada AJAX ou um formulário tradicional)
-        form.submit();
-    });
-    alert("Formulário enviado com sucesso!");
-
-
-// pegar o botão Limpar pelo seu nome
-const resetButton = document.querySelector('button[name="Annuler"]');
-
-// adicionar um evento de clique ao botão Limpar
-resetButton.addEventListener("click", function(event) {
-    // aqui você pode adicionar sua lógica para limpar os campos do formulário
-    form.reset(); // limpar os campos do formulário
+    let valider = true;
+    if (!choixTelProvince(telephone.value, province.value)) {
+        valider = false;
+        document.querySelector("#province + .erreur").classList.remove("invisible");
+    }
+    if (!differencePommPatate(patate.value, pomme.value)) {
+        valider = false;
+        document.querySelector("#commande .erreur").classList.remove("invisible");
+    }
+    if (!valider) {
+        evenement.preventDefault();
+    }
+    else {
+        document.getElementById("erreur_tel").setAttribute("class", "");
+        return false;
+    }
 });
-
-
-//Primeiro, selecionamos o formulário usando o ID "contact".
-// Adicionamos um ouvinte de evento de submit ao formulário. Isso significa que a função dentro do addEventListener será executada quando o formulário for enviado.
-// Dentro da função do ouvinte de evento, usamos "event.preventDefault()" para evitar o comportamento padrão de submit (recarregar a página).
-// Em seguida, validamos se todos os campos obrigatórios foram preenchidos usando a verificação
-// "if (nome == "" || email == "" || telefone == "" || company == "" || province == "" || Pommes == "" || Patates == "" || Pseudonyme == "" || Commentaires == "")" e, se algum campo não estiver preenchido, most
